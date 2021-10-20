f7cf3bdcb48a4160a0bc76b97e80ece8eea98352
Nandor Devai <mail@nandordevai.net>
2021-10-20 11:09:59 +0200

    add index & simple chart

--- layered.js
+++ layered.js
@@ -1,15 +1,3 @@
-function hsl(h, s, l) {
-    return `hsl(${h}, ${s * 100}%, ${l * 100}%)`;
-}
-
-const h = 200;
-const s = .6;
-const l = .4;
-const colors = {
-    meteor: hsl(h + 120, s, l),
-    corn: hsl(h, s, l),
-};
-
 const schema = {
     $schema: 'https://vega.github.io/schema/vega-lite/v5.json',
     config: {
@@ -84,5 +72,3 @@ const schema = {
         },
     },
 };
-
-vegaEmbed('#graph', schema);
