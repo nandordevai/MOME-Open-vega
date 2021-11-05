100a3b59376aa0ed8eafdf15c8c639598fa17bd1
Nandor Devai <mail@nandordevai.net>
2021-11-05 12:51:55 +0100

    simplify API example

--- layered-api.js
+++ layered-api.js
@@ -1,118 +1,32 @@
-const schema = {
-    $schema: 'https://vega.github.io/schema/vega-lite/v5.json',
-    config: {
-        axis: {
-            labelFontSize: 12,
-            titleFontSize: 14,
-            titleFontWeight: 'normal',
-        },
-    },
-    data: {
-        url: 'data.csv',
-    },
-    width: 800,
-    height: 500,
-    encoding: {
-        x: {
-            field: 'year',
-            type: 'temporal',
-            timeUnit: 'year',
-            axis: {
-                tickCount: 'year',
-                title: null,
-            },
-        },
-    },
-    layer: [
-        {
-            mark: {
-                type: 'line',
-                interpolate: 'monotone',
-                color: colors.meteor,
-            },
-            encoding: {
-                y: {
-                    field: 'meteors',
-                    type: 'quantitative',
-                    axis: {
-                        title: 'Total meteors observed',
-                        titleColor: colors.meteor,
-                    },
-                    scale: {
-                        domain: [20000, 120000],
-                    },
-                    orient: 'left',
-                },
-            },
-        },
-        {
-            mark: {
-                type: 'line',
-                interpolate: 'monotone',
-                color: colors.corn,
-            },
-            encoding: {
-                y: {
-                    field: 'corn',
-                    type: 'quantitative',
-                    axis: {
-                        title: 'Fresh sweet corn (lbs per capita)',
-                        titleColor: colors.corn,
-                    },
-                    scale: {
-                        domain: [5.0, 10.0],
-                    },
-                },
-            },
-        }
-    ],
-    resolve: {
-        scale: {
-            y: 'independent',
-        },
-    },
-};
-
 const options = {
     view: {
         renderer: "canvas",
     },
 };
 
+function makeLayer(field, title, color, domain) {
+    return vl.markLine({
+        interpolate: 'monotone',
+    })
+        .encode(
+            vl.x().field('year').type('temporal').timeUnit('year').axis({
+                tickCount: 'year',
+                title: null,
+            }),
+            vl.y().field(field).type('quantitative').axis({
+                title,
+                titleColor: color,
+            }).scale({
+                domain,
+            }),
+            vl.color().value(color),
+        );
+}
+
 vl.register(vega, vegaLite, options);
 vl.layer(
-    vl.markLine({
-        interpolate: 'monotone',
-    })
-        .encode(
-            vl.x().field('year').type('temporal').timeUnit('year').axis({
-                tickCount: 'year',
-                title: null,
-            }),
-            vl.y().field('meteors').type('quantitative').axis({
-                title: 'Total meteors observed',
-                titleColor: colors.meteor,
-            }).scale({
-                domain: [20000, 120000],
-            }),
-            vl.color().value(colors.meteor),
-        ),
-    vl.markLine({
-        interpolate: 'monotone',
-    })
-        .encode(
-            vl.x().field('year').type('temporal').timeUnit('year').axis({
-                tickCount: 'year',
-                title: null,
-            }),
-            vl.y().field('corn').type('quantitative').axis({
-                title: 'Fresh sweet corn (lbs per capita)',
-                titleColor: colors.corn,
-            }).scale({
-                domain: [5.0, 10.0],
-            }),
-            vl.color().value(colors.corn),
-        )
+    makeLayer('meteors', 'Total meteors observed', colors.meteor, [20000, 120000]),
+    makeLayer('corn', 'Fresh sweet corn (lbs per capita)', colors.corn, [5.0, 10.0]),
 )
     .data('data.csv')
     .config({
