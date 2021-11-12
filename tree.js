a4c133bf5796bbe050cd28badf0ebf4ab70bfc4f
Nandor Devai <mail@nandordevai.net>
2021-11-12 11:31:42 +0100

    finish tree

--- tree.js
+++ tree.js
@@ -1,123 +1,127 @@
 const schema = {
-    "$schema": "https://vega.github.io/schema/vega/v5.json",
-    "data": [
+    $schema: 'https://vega.github.io/schema/vega/v5.json',
+    data: [
         {
-            "name": "tree",
-            "url": "https://raw.githubusercontent.com/nandordevai/MOME-Open-vega/main/top-1000-extended-posters.json",
-            "transform": [
-                { "type": "filter", "expr": "datum.year > 2016 && datum.metascore !== ''" },
-                { "type": "nest", "keys": ["year", "genre"], "generate": true },
+            name: 'tree',
+            url: 'https://raw.githubusercontent.com/nandordevai/MOME-Open-vega/main/top-1000-extended-posters.json',
+            transform: [
                 {
-                    "type": "tree",
-                    "method": "tidy",
-                    "separation": true,
-                    "as": ["y", "x", "depth", "children"],
-                    "size": [1200, 800]
-                }
-            ]
+                    type: 'filter',
+                    expr: 'datum.year > 2016 && datum.metascore !== ""',
+                },
+                { type: 'nest', keys: ['year', 'genre'], generate: true },
+                {
+                    type: 'tree',
+                    method: 'tidy',
+                    separation: true,
+                    as: ['y', 'x', 'depth', 'children'],
+                    size: [1200, 800],
+                },
+            ],
         },
         {
-            "name": "links",
-            "source": "tree",
-            "transform": [
-                { "type": "treelinks" },
-                { "type": "linkpath", "orient": "horizontal", "shape": "diagonal" }
-            ]
+            name: 'links',
+            source: 'tree',
+            transform: [
+                { type: 'treelinks' },
+                { type: 'linkpath', orient: 'horizontal', shape: 'diagonal' },
+            ],
         },
         {
-            "name": "nodes",
-            "source": "tree",
-            "transform": [
-                { "type": "filter", "expr": "datum.children > 0" }
-            ]
+            name: 'nodes',
+            source: 'tree',
+            transform: [{ type: 'filter', expr: 'datum.children > 0' }],
         },
         {
-            "name": "leaves",
-            "source": "tree",
-            "transform": [
-                { "type": "filter", "expr": "datum.children == 0" }
-            ]
-        }
+            name: 'leaves',
+            source: 'tree',
+            transform: [{ type: 'filter', expr: 'datum.children == 0' }],
+        },
     ],
-    "marks": [
+    scales: [
         {
-            "type": "path",
-            "from": { "data": "links" },
-            "encode": {
-                "update": {
-                    "path": { "field": "path" },
-                    "stroke": { "value": "#ccc" },
-                    "strokeWidth": {
-                        "field": "target.metascore",
-                        "mult": 0.05
-                    }
-                }
-            }
-        },
-        {
-            "type": "symbol",
-            "from": { "data": "nodes" },
-            "encode": {
-                "enter": {
-                    "size": {
-                        "value": 100
-                    },
-                    "stroke": { "value": "hsla(0, 0%, 40%, 1)" }
-                },
-                "update": {
-                    "x": { "field": "x" },
-                    "y": { "field": "y" },
-                    "fill": { "value": "hsla(120, 25%, 60%, 1)" }
-                }
-            }
-        },
-        {
-            "type": "symbol",
-            "from": { "data": "leaves" },
-            "encode": {
-                "enter": {
-                    "size": {
-                        "field": "gross_usd",
-                        "mult": 0.000003
-                    },
-                    "stroke": { "value": "hsla(0, 0%, 40%, 1)" }
-                },
-                "update": {
-                    "x": { "field": "x" },
-                    "y": { "field": "y" },
-                    "fill": { "value": "hsla(120, 25%, 60%, 1)" }
-                }
-            }
-        },
-        {
-            "type": "text",
-            "from": {
-                "data": "leaves"
+            name: 'grossusd',
+            type: 'linear',
+            domain: {
+                data: 'tree',
+                field: 'gross_usd',
             },
-            "encode": {
-                "enter": {
-                    "x": { "field": "x", "offset": 20 },
-                    "y": { "field": "y", "offset": 4 },
-                    "text": {
-                        "field": "title_hu"
-                    }
-                }
-            }
+            range: [5, 1000],
         },
         {
-            "type": "text",
-            "from": {
-                "data": "nodes"
+            name: 'metascore',
+            type: 'log',
+            domain: {
+                data: 'tree',
+                field: 'metascore',
             },
-            "encode": {
-                "enter": {
-                    "x": { "field": "x", "offset": 10 },
-                    "y": { "field": "y", "offset": 4 },
-                    "text": {
-                        "field": "key"
-                    }
-                }
-            }
-        }
-    ]
+            range: [1, 10],
+        },
+    ],
+    marks: [
+        {
+            type: 'path',
+            from: { data: 'links' },
+            encode: {
+                enter: {
+                    path: { field: 'path' },
+                    stroke: { value: 'hsla(0, 0%, 80%, 1)' },
+                    strokeWidth: {
+                        field: 'target.metascore',
+                        scale: 'metascore',
+                    },
+                },
+            },
+        },
+        {
+            type: 'symbol',
+            from: { data: 'nodes' },
+            encode: {
+                enter: {
+                    size: { value: 100 },
+                    stroke: { value: 'hsla(0, 0%, 40%, 1)' },
+                    x: { field: 'x' },
+                    y: { field: 'y' },
+                    fill: { value: 'hsla(120, 25%, 60%, 1)' },
+                },
+            },
+        },
+        {
+            type: 'symbol',
+            from: { data: 'leaves' },
+            encode: {
+                enter: {
+                    size: { field: 'gross_usd', scale: 'grossusd' },
+                    stroke: { value: 'hsla(0, 0%, 40%, 1)' },
+                },
+                update: {
+                    x: { field: 'x' },
+                    y: { field: 'y' },
+                    fill: { value: 'hsla(120, 25%, 60%, 1)' },
+                },
+            },
+        },
+        {
+            type: 'text',
+            from: { data: 'leaves' },
+            encode: {
+                enter: {
+                    x: { field: 'x', offset: 20 },
+                    y: { field: 'y', offset: 4 },
+                    text: { field: 'title_hu' },
+                },
+            },
+        },
+        {
+            type: 'text',
+            from: { data: 'nodes' },
+            encode: {
+                enter: {
+                    x: { field: 'x', offset: 10 },
+                    y: { field: 'y', offset: 4 },
+                    text: { field: 'key' },
+                },
+            },
+        },
+    ],
 };
